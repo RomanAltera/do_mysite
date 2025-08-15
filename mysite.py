e00f52845983ce993ce33a0f04c6a7a36a153374
roman sidorov <roman@MacBook-Air-roman.local>
2025-08-15 14:02:21 +0300

    изменение текста приветствия

--- mysite.py
+++ mysite.py
@@ -1,10 +1,10 @@
-#begin program
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Это теперь мой новый сайт!!!Мой первый новый сайт</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
