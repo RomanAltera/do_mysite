8b1422d5e9ab3a1495bc9d07f73c1fc7fad98a2e
roman sidorov <roman@MacBook-Air-roman.local>
2025-08-16 19:40:22 +0300

    добавление шапки сайта+нижней меню и страниы со статьями

--- mysite.py
+++ mysite.py
@@ -1,10 +1,19 @@
-
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Это теперь мой новый сайт!!!Мой первый новый сайт</h1>"
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/articles")
+def articles():
+    return render_template("articles.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
+    #app.run(host='0.0.0.0')
